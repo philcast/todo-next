7531e2ec3dcba2efed0c67fc4f88764d30367db8
philcast <3006367+philcast@users.noreply.github.com>
2023-06-07 21:50:00 +0000

    feat(header): update avatar image

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,11 +14,11 @@ export function Header({ title }: HeaderProps) {
           {title}
         </Typography>
         <Avatar
-            size="md"
-            variant="circular"
-            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
-            alt="candice wu"
-          />
+          size="md"
+          variant="circular"
+          src="https://gravatar.com/userimage/35548340/749da3d55ec4acd802a164d120bf0982?size=100"
+          alt="philippe"
+        />
       </div>
     </Navbar>
   );
