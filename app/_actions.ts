4eb15104a4e363b44587e654d6b313abd8d40475
philcast <3006367+philcast@users.noreply.github.com>
2023-06-07 21:49:22 +0000

    feat(todos-list): deletion action

--- app/_actions.ts
+++ app/_actions.ts
@@ -3,12 +3,21 @@
 import { PrismaClient } from '@prisma/client';
 import { revalidatePath } from 'next/cache';
 const prisma = new PrismaClient();
- 
+
 export async function addTodoList(title: string) {
   await prisma.todoList.create({
     data: {
       title: title,
     },
   });
-  revalidatePath('/');
+  revalidatePath('/lists');
+}
+
+export async function removeTodoList(id: string) {
+  await prisma.todoList.delete({
+    where: {
+      id,
+    },
+  });
+  revalidatePath('/lists');
 }
